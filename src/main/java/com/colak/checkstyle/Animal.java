4b9e74b27627ed793ef80ff2b0b3599a34f17d4f
Orçun Çolak <orcun.colak@hazelcast.com>
2024-09-02 15:08:25 +0300

    Initial commit

---
+++ src/main/java/com/colak/checkstyle/Animal.java
@@ -0,0 +1,9 @@
+package com.colak.checkstyle;
+
+public class Animal {
+    
+    public void eat(String food) {
+        if("meat".equals(food))
+            System.out.println("Animal is eating meat");
+    }
+}
